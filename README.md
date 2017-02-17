a510e3de64a5002839eaef21e0c51aee26848f0d
Dimitri Benin <dimitri.benin@it-designers.de>
2017-02-17 18:00:08 +0100

    Implement minimal working version. Took most code from npm-run-all.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# run-script
+# yarpm
 CLI tool to run npm scripts with either npm or yarn, depending on how it was started
---
+++ bin/yarpm.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+'use strict';
+
+const yarpm = require('../lib');
+
+const argv = process.argv.slice(2);
+
+yarpm(argv, {stdin: process.stdin, stdout: process.stdout, stderr: process.stderr})
+// Not sure why, but sometimes the process never exits on Git Bash (MINGW64)
+  .then(() => process.exit(0))
+  .catch(() => process.exit(1));
